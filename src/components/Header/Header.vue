c10a2d13eb48fc03a3332a95c76ae52397d52940
Corey H <rewer6@gmail.com>
2018-10-15 00:25:52 -0500

    Removed more artifact code

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,12 +1,12 @@
 <template>
   <nav class="dnb-global-header header-utility-btns navbar navbar-default">
-      <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <a class="navbar-brand mx-auto" href="#"><span class="sr-only">D&amp;B Blockchain Ticker Tracker</span></a>
-        </div>
-      </div><!-- /.container-fluid -->
-    </nav>
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <a class="navbar-brand mx-auto" href="#"><span class="sr-only">D&amp;B Blockchain Ticker Tracker</span></a>
+      </div>
+    </div><!-- /.container-fluid -->
+  </nav>
 </template>
 
 <script>
